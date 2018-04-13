231525e37918ac380fbb13c5de0f03373c2d23ef
zoulvzhou <940933073@qq.com>
2018-04-13 19:43:50 +0800

    SpringBoot   和 Redis

--- src/main/java/com/example/springbootredis/repository/PersonRepositoryImpl.java
+++ src/main/java/com/example/springbootredis/repository/PersonRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.example.springbootredis.repository;
 
 import com.example.springbootredis.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
@@ -11,18 +13,40 @@ public class PersonRepositoryImpl implements PersonRepository {
 
     private final Map<String, Person> repository = new HashMap<>();
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+//    @Override
+//    public Person findPerson(String id) {
+//
+//
+//        return repository.get(id);
+//    }
+
 
     @Override
     public Person findPerson(String id) {
 
 
-        return repository.get(id);
+        return (Person) redisTemplate.opsForValue().get(id);
     }
+
+
+//    @Override
+//    public boolean savePerson(Person person) {
+//        return repository.putIfAbsent(person.getId(), person) == null;
+//    }
+
 
     @Override
     public boolean savePerson(Person person) {
-        return repository.putIfAbsent(person.getId(), person) == null;
+
+        redisTemplate.opsForValue().set(person.getId(), person);
+
+        return true;
     }
+
 }
 
 
